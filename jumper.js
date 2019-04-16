e3508577e91d424d414ef1342789cc20556eb70f
junhhe <42394094+junhhe@users.noreply.github.com>
2019-04-16 12:51:32 -1000

    Update jumper.js

--- jumper.js
+++ jumper.js
@@ -6,7 +6,7 @@ function jumper() {
       this.velocity = 0; // speed of the gravity
       
       this.show = function() {
-        image(img, 50,50);
+        image(img, this.x,this.y);
       }
       this.move = function(){
         if ((keyIsDown(65)) && (this.x > 50)){
